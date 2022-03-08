c23fd505040ecf4f12ec20d60ed8814d7cb8e5d5
Juan Pablo Benitez <89710183+juanbenitez1@users.noreply.github.com>
2022-03-08 14:26:26 -0300

    Update granos_dcac.py

--- granos_dcac.py
+++ granos_dcac.py
@@ -41,16 +41,19 @@ time.sleep(4)
 data_maiz_usd = driver.find_elements_by_tag_name('td')
 dmu = []
 for d in data_maiz_usd:
-    dmu.append(d.text)
-desc_m = [dmu[0],dmu[4]]
-prec_m_usd = [dmu[1],dmu[5]]
-time.sleep(2)
-driver.find_element_by_xpath('//*[@id="vue-app"]/div/div[2]/div[1]/div[2]/span/button[1]').click()
-data_maiz_pes = driver.find_elements_by_tag_name('td')
-dmp = []
-for d in data_maiz_pes:
-    dmp.append(d.text)
-prec_m_pes = [dmp[1],dmp[5]]
+    try:
+        dmu.append(d.text)
+    desc_m = [dmu[0],dmu[4]]
+    prec_m_usd = [dmu[1],dmu[5]]
+    time.sleep(2)
+    driver.find_element_by_xpath('//*[@id="vue-app"]/div/div[2]/div[1]/div[2]/span/button[1]').click()
+    data_maiz_pes = driver.find_elements_by_tag_name('td')
+    dmp = []
+    for d in data_maiz_pes:
+        dmp.append(d.text)
+    prec_m_pes = [dmp[1],dmp[5]]
+    except:
+        dmu.append('ND')
 
 time.sleep(5)
 
@@ -60,46 +63,65 @@ driver.find_element_by_xpath("/html/body/div[6]/div[1]/div[2]/div/div[1]/div/div
 time.sleep(4)
 
 data_soja_usd = driver.find_elements_by_tag_name('td')
-time.sleep(2)
+time.sleep(5)
 dsu = []
 for d in data_soja_usd:
-    dsu.append(d.text)
-desc_s = [dsu[0],dsu[4]]
-prec_s_usd = [dsu[1],dsu[5]]
-time.sleep(2)
-driver.find_element_by_xpath('/html/body/div[6]/div[1]/div[2]/div/div[1]/div/div[2]/div[1]/div[2]/span/button[1]').click()
-data_soja_pes = driver.find_elements_by_tag_name('td')
-dsp = []
-for d in data_soja_pes:
-    dsp.append(d.text)
-prec_s_pes = [dsp[1],dsp[5]]
-
+    try:
+        dsu.append(d.text)
+    desc_s = [dsu[0],dsu[4]]
+    prec_s_usd = [dsu[1],dsu[5]]
+    time.sleep(5)
+    driver.find_element_by_xpath('/html/body/div[6]/div[1]/div[2]/div/div[1]/div/div[2]/div[1]/div[2]/span/button[1]').click()
+    data_soja_pes = driver.find_elements_by_tag_name('td')
+    dsp = []
+    for d in data_soja_pes:
+        dsp.append(d.text)
+    prec_s_pes = [dsp[1],dsp[5]]
+    except:
+        dsu.append('ND')
+        
 time.sleep(5)
+
 driver.find_element_by_xpath("/html/body/div[6]/div[1]/div[2]/div/div[1]/div/div[2]/div[1]/div[1]/button[3]").click()
-time.sleep(3)
+time.sleep(5)
 driver.find_element_by_xpath("/html/body/div[6]/div[1]/div[2]/div/div[1]/div/div[2]/div[1]/div[2]/span/button[2]").click()
-time.sleep(4)
+time.sleep(5)
+
 data_trigo_usd = driver.find_elements_by_tag_name('td')
-time.sleep(2)
+time.sleep52)
 dtu = []
 for d in data_trigo_usd:
-    dtu.append(d.text)
-desc_t = [dtu[0],dtu[8]]
-prec_t_usd = [dtu[1],dtu[9]]
-time.sleep(2)
-driver.find_element_by_xpath('/html/body/div[6]/div[1]/div[2]/div/div[1]/div/div[2]/div[1]/div[2]/span/button[1]').click()
-data_trigo_pes = driver.find_elements_by_tag_name('td')
-dtp = []
-for d in data_trigo_pes:
-    dtp.append(d.text)
-prec_t_pes = [dtp[1],dtp[9]]
+    try:
+        dtu.append(d.text)
+    desc_t = [dtu[0],dtu[8]]
+    prec_t_usd = [dtu[1],dtu[9]]
+    time.sleep(5)
+    driver.find_element_by_xpath('/html/body/div[6]/div[1]/div[2]/div/div[1]/div/div[2]/div[1]/div[2]/span/button[1]').click()
+    data_trigo_pes = driver.find_elements_by_tag_name('td')
+    dtp = []
+    for d in data_trigo_pes:
+        dtp.append(d.text)
+    prec_t_pes = [dtp[1],dtp[9]]
+    except:
+        dtu.append('ND')
 
-m = {'descripcion':desc_m,'pesosxtn':prec_m_pes,'usdxtn':prec_m_usd}
-maiz = pd.DataFrame(data=m)
-s = {'descripcion':desc_s,'pesosxtn':prec_s_pes,'usdxtn':prec_s_usd}
-soja = pd.DataFrame(data=s)
-t = {'descripcion':desc_t,'pesosxtn':prec_t_pes,'usdxtn':prec_t_usd}
-trigo = pd.DataFrame(data=t)
+if dmu[0] != 'ND':
+    m = {'descripcion':desc_m,'pesosxtn':prec_m_pes,'usdxtn':prec_m_usd}
+    maiz = pd.DataFrame(data=m)
+else:
+    pass
+
+if dsu[0] != 'ND':
+    s = {'descripcion':desc_s,'pesosxtn':prec_s_pes,'usdxtn':prec_s_usd}
+    soja = pd.DataFrame(data=s)
+else:
+    pass
+
+if dtu[0] != 'ND':
+    t = {'descripcion':desc_t,'pesosxtn':prec_t_pes,'usdxtn':prec_t_usd}
+    trigo = pd.DataFrame(data=t)
+else:
+    pass
 
 granos = pd.concat([maiz,soja,trigo])
 granos
@@ -116,7 +138,7 @@ granos['pesosxtn'] = granos['pesosxtn'].map(lambda x: str(x).replace('.','')).as
 granos['tipo_cambio'] = granos['pesosxtn']/granos['usdxtn']
 granos
 
-time.sleep(0.5)
+time.sleep(5)
 driver.quit()
 
 gc = pygsheets.authorize(service_file='creds.json')
