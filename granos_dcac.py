af0db0a226c0380b1787139919262b6abd64943a
Juan Pablo Benitez <89710183+juanbenitez1@users.noreply.github.com>
2022-03-09 09:15:40 -0300

    Update granos_dcac.py

--- granos_dcac.py
+++ granos_dcac.py
@@ -41,19 +41,17 @@ time.sleep(4)
 data_maiz_usd = driver.find_elements_by_tag_name('td')
 dmu = []
 for d in data_maiz_usd:
-    try:
-        dmu.append(d.text)
-        desc_m = [dmu[0],dmu[4]]
-        prec_m_usd = [dmu[1],dmu[5]]
-        time.sleep(2)
-        driver.find_element_by_xpath('//*[@id="vue-app"]/div/div[2]/div[1]/div[2]/span/button[1]').click()
-        data_maiz_pes = driver.find_elements_by_tag_name('td')
-        dmp = []
-        for d in data_maiz_pes:
-            dmp.append(d.text)
-        prec_m_pes = [dmp[1],dmp[5]]
-    except:
-        dmu.append('ND')
+    dmu.append(d.text)
+desc_m = [dmu[0],dmu[4]]
+prec_m_usd = [dmu[1],dmu[5]]
+time.sleep(2)
+driver.find_element_by_xpath('//*[@id="vue-app"]/div/div[2]/div[1]/div[2]/span/button[1]').click()
+data_maiz_pes = driver.find_elements_by_tag_name('td')
+dmp = []
+
+for d in data_maiz_pes:
+    dmp.append(d.text)
+prec_m_pes = [dmp[1],dmp[5]]
 
 time.sleep(5)
 
@@ -65,20 +63,19 @@ time.sleep(4)
 data_soja_usd = driver.find_elements_by_tag_name('td')
 time.sleep(5)
 dsu = []
+
 for d in data_soja_usd:
-    try:
-        dsu.append(d.text)
-        desc_s = [dsu[0],dsu[4]]
-        prec_s_usd = [dsu[1],dsu[5]]
-        time.sleep(5)
-        driver.find_element_by_xpath('/html/body/div[6]/div[1]/div[2]/div/div[1]/div/div[2]/div[1]/div[2]/span/button[1]').click()
-        data_soja_pes = driver.find_elements_by_tag_name('td')
-        dsp = []
-        for d in data_soja_pes:
-            dsp.append(d.text)
-        prec_s_pes = [dsp[1],dsp[5]]
-    except:
-        dsu.append('ND')
+    dsu.append(d.text)
+desc_s = [dsu[0],dsu[4]]
+prec_s_usd = [dsu[1],dsu[5]]
+time.sleep(5)
+driver.find_element_by_xpath('/html/body/div[6]/div[1]/div[2]/div/div[1]/div/div[2]/div[1]/div[2]/span/button[1]').click()
+data_soja_pes = driver.find_elements_by_tag_name('td')
+dsp = []
+
+for d in data_soja_pes:
+    dsp.append(d.text)
+prec_s_pes = [dsp[1],dsp[5]]
         
 time.sleep(5)
 
@@ -91,40 +88,26 @@ data_trigo_usd = driver.find_elements_by_tag_name('td')
 time.sleep(5)
 dtu = []
 for d in data_trigo_usd:
-    try:
-        dtu.append(d.text)
-        desc_t = [dtu[0],dtu[8]]
-        prec_t_usd = [dtu[1],dtu[9]]
-        time.sleep(5)
-        driver.find_element_by_xpath('/html/body/div[6]/div[1]/div[2]/div/div[1]/div/div[2]/div[1]/div[2]/span/button[1]').click()
-        data_trigo_pes = driver.find_elements_by_tag_name('td')
-        dtp = []
-        for d in data_trigo_pes:
-            dtp.append(d.text)
-        prec_t_pes = [dtp[1],dtp[9]]
-    except:
-        dtu.append('ND')
+    dtu.append(d.text)
+desc_t = [dtu[0],dtu[8]]
+prec_t_usd = [dtu[1],dtu[9]]
+time.sleep(5)
+driver.find_element_by_xpath('/html/body/div[6]/div[1]/div[2]/div/div[1]/div/div[2]/div[1]/div[2]/span/button[1]').click()
+data_trigo_pes = driver.find_elements_by_tag_name('td')
+dtp = []
 
-if dmu[0] != 'ND':
-    m = {'descripcion':desc_m,'pesosxtn':prec_m_pes,'usdxtn':prec_m_usd}
-    maiz = pd.DataFrame(data=m)
-else:
-    m = {'descripcion':'ND','pesosxtn':'ND','usdxtn':'ND'}
-    maiz = pd.DataFrame(data=m)
+for d in data_trigo_pes:
+    dtp.append(d.text)
+prec_t_pes = [dtp[1],dtp[9]]
 
-if dsu[0] != 'ND':
-    s = {'descripcion':desc_s,'pesosxtn':prec_s_pes,'usdxtn':prec_s_usd}
-    soja = pd.DataFrame(data=s)
-else:
-    s = {'descripcion':'ND','pesosxtn':'ND','usdxtn':'ND'}
-    soja = pd.DataFrame(data=s)
+m = {'descripcion':desc_m,'pesosxtn':prec_m_pes,'usdxtn':prec_m_usd}
+maiz = pd.DataFrame(data=m)
 
-if dtu[0] != 'ND':
-    t = {'descripcion':desc_t,'pesosxtn':prec_t_pes,'usdxtn':prec_t_usd}
-    trigo = pd.DataFrame(data=t)
-else:
-    t = {'descripcion':'ND','pesosxtn':'ND','usdxtn':'ND'}
-    trigo = pd.DataFrame(data=t)
+s = {'descripcion':desc_s,'pesosxtn':prec_s_pes,'usdxtn':prec_s_usd}
+soja = pd.DataFrame(data=s)
+
+t = {'descripcion':desc_t,'pesosxtn':prec_t_pes,'usdxtn':prec_t_usd}
+trigo = pd.DataFrame(data=t)
 
 granos = pd.concat([maiz,soja,trigo])
 granos
@@ -135,19 +118,12 @@ fecha = str(fecha.strftime('%d/%m/%Y'))
 granos['Fecha'] = fecha
 granos = granos.reindex(columns=['Fecha','descripcion','pesosxtn','usdxtn'])
 granos['usdxtn'] = granos['usdxtn'].squeeze()
-for i in granos['usdxtn']:
-    if i != 'ND':
-    i = i.map(lambda x: str(x).replace(',','.')).astype(float)
-else:
-    pass
+granos['usdxtn'] = granos['usdxtn'].map(lambda x: str(x).replace(',','.')).astype(float)
 
 granos['pesosxtn'] = granos['pesosxtn'].squeeze()
-for i in granos['pesosxtn']:
-    if i != 'ND':
-        i = i.map(lambda x: str(x).replace('.','')).astype(float)
-        granos['tipo_cambio'] = granos['pesosxtn']/granos['usdxtn']
-    else:
-        granos['tipo_cambio'] = 'ND'
+granos['pesosxtn'] = granos['pesosxtn'].map(lambda x: str(x).replace('.','')).astype(float)
+
+granos['tipo_cambio'] = granos['pesosxtn']/granos['usdxtn']
 # granos
 
 time.sleep(5)
